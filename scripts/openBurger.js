cea6cc1fd2af7a0a45963f08dc891f52c3131e00
Andrey <butic.92@mail.ru>
2021-11-01 21:21:42 +0300

    complete theme switch, small changes in html/css

--- scripts/openBurger.js
+++ scripts/openBurger.js
@@ -46,14 +46,14 @@ function closeBurger(){
 // change theme script
 let header = document.querySelector('.header');
 let aboutImg = document.querySelectorAll('.about__img');
-
+let topicImg = document.querySelector('.topic__img');
 
 links[2].onclick = function(){
   this.classList.toggle('special');
   if(this.classList.contains('special')){
     header.style.backgroundImage = "url(img/bg1.jpg)";
     burger.src = "img/menu.jpg";
-
+    topicImg.src = "img/7.jpg";
     for(let i = 0; i<portfolios.length; i++){
       let imageLink = "url(img/"+i+".jpg)";
       portfolios[i].style.backgroundImage = imageLink;
@@ -67,6 +67,7 @@ links[2].onclick = function(){
   else{
     header.style.backgroundImage = null;
     burger.src = "img2/menu.png";
+    topicImg.src = "img2/10.jpeg";
     for(let i = 0; i<portfolios.length; i++){
       portfolios[i].style.backgroundImage = null;
       if(i<6){
