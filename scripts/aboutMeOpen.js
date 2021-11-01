057e1fd766ce72bf007b9cb67451c82a8d4796ec
Andrey <butic.92@mail.ru>
2021-11-01 19:45:42 +0300

    added scripts, changes in css-focus, feedback button, added template in html, comments in css

---
+++ scripts/aboutMeOpen.js
@@ -0,0 +1,56 @@
+let body = document.querySelector('body');
+let temp = document.querySelector('.template').content;
+let tempBG = temp.querySelector('.template__button');
+let tempClose = temp.querySelector('.template__container-close');
+let tempFrame = temp.querySelector('.template__body');
+let tempImg = temp.querySelector('.template__img');
+let tempHeader = temp.querySelector('.template__header');
+let tempText = temp.querySelector('.template__text');
+let portfolios = document.querySelectorAll('.main__portfolio');
+
+function closeDom(frame, closeBG, closeX){
+  closeBG.onclick = function(){
+    frame.remove();
+    closeBG.remove();
+    body.style = null;
+    removeTempImgStyle();
+  }
+  closeX.onclick = function(){
+    frame.remove();
+    closeBG.remove();
+    body.style = null;
+    removeTempImgStyle();
+  }
+}
+
+function removeTempImgStyle(){
+    tempImg.style = null;
+    setTimeout(function(){
+      tempImg.classList.remove('absolute');
+      tempFrame.style = null;
+    }, 500);
+}
+
+tempImg.onclick = function(){
+  if(!tempImg.classList.contains('absolute')){
+    tempImg.classList.toggle('absolute');
+    tempImg.style = "height: 100%; width:101.5%; border-radius: 0 0 20px 20px; top:3.2vh; bottom:0; visibility:visible;";
+    tempFrame.style.color = "darkgray";
+  }
+  else{
+    removeTempImgStyle();
+  }
+  
+}
+
+  for(let portfolio of portfolios){
+   portfolio.onclick = function(){
+      tempImg.src = document.defaultView.getComputedStyle(portfolio, null).backgroundImage.split('"')[1];
+      tempHeader.innerText = portfolio.querySelector('.main__portfolio-text').textContent;
+      tempText.innerText = portfolio.querySelector('.main__portfolio-text').textContent;
+     body.style.overflow = "hidden";
+     body.appendChild(tempBG);
+     body.appendChild(tempFrame);
+     closeDom(tempFrame, tempBG, tempClose);
+   }
+  }
---
+++ scripts/openBurger.js
@@ -0,0 +1,44 @@
+let nav = document.querySelector('.header__nav');
+let burger = nav.querySelector('.header__burger');
+let list = nav.querySelector('.header__links');
+let links = list.querySelectorAll('.header__link');
+
+window.onclick = function(e){
+  if(e.target.classList.contains('header__nav')||e.target.classList.contains('header__burger')){
+    checkBurger();
+  }
+  else if(list.classList.contains('visible')){
+    closeBurger();
+  }
+};
+
+function checkBurger(){
+  if(!list.classList.contains('visible')){
+    openBurger();
+  }
+  else{
+     closeBurger();
+  }
+};
+
+function openBurger(){
+   list.classList.add('visible');
+   burger.style = "transform: rotate(180deg);";
+   for(let i = 0; i<links.length; i++){
+     openLinks(i);
+   }
+};
+
+function openLinks(i){
+  setTimeout(function(){
+    links[i].style.opacity = "1";
+  }, 333*i);
+};
+
+function closeBurger(){
+  for(let link of links){
+    link.style = null;
+  }
+    list.classList.remove('visible');
+    burger.style = "transform: rotate(0deg);";
+};
