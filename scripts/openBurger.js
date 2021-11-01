b10c832fac85eb85430590e82a6af031d4336aa6
Andrey <butic.92@mail.ru>
2021-11-01 21:11:48 +0300

    change thme updated (inside open burger)

--- scripts/openBurger.js
+++ scripts/openBurger.js
@@ -42,9 +42,36 @@ function closeBurger(){
     list.classList.remove('visible');
     burger.style = "transform: rotate(0deg);";
 }
+
+// change theme script
+let header = document.querySelector('.header');
+let aboutImg = document.querySelectorAll('.about__img');
+
+
 links[2].onclick = function(){
-  for(let i = 0; i<portfolios.length; i++){
-      portfolios[i].style.backgroundImage = "url(../img/"+i+".jpg)";
+  this.classList.toggle('special');
+  if(this.classList.contains('special')){
+    header.style.backgroundImage = "url(img/bg1.jpg)";
+    burger.src = "img/menu.jpg";
+
+    for(let i = 0; i<portfolios.length; i++){
+      let imageLink = "url(img/"+i+".jpg)";
+      portfolios[i].style.backgroundImage = imageLink;
+      if(i<6){
+        aboutImg[i].style.backgroundImage = imageLink;
+      }
+    }
+
+    
+  } 
+  else{
+    header.style.backgroundImage = null;
+    burger.src = "img2/menu.png";
+    for(let i = 0; i<portfolios.length; i++){
+      portfolios[i].style.backgroundImage = null;
+      if(i<6){
+        aboutImg[i].style.backgroundImage = null;
+      }
+    }
   }
 }
-
--- scripts/specialTheme.js
+++
@@ -1,5 +0,0 @@
-links[2].onclick = function(){
-    for(let i = 0; i<portfolios.length; i++){
-        portfolios[i].style = "color: red;";
-    }
-  }
