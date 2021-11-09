749c52998534125f72a761efd7ab5798c70d8187
Andrey <butic.92@mail.ru>
2021-11-09 04:19:04 +0300

    change px to open up-button in js

--- js/buttonTop.js
+++ js/buttonTop.js
@@ -1,7 +1,6 @@
 let arrowTop = body.querySelector('.top__arrow');
-
 window.onscroll = function(){
-    if(document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
+    if(document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight){
         arrowTop.style = "transform: rotateX(360deg); cursor:pointer;"
     }
     else{
